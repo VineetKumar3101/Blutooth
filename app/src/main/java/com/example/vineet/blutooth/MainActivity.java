38119f2460bf2f6df9d84f8ae157a19a3dc80557
Vineet Kumar <vineet.kumar_cs20@gla.ac.in>
2022-01-05 13:06:06 +0530

    Initial commit

---
+++ app/src/main/java/com/example/vineet/blutooth/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.vineet.blutooth;
+
+import android.bluetooth.BluetoothAdapter;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+Button b1,b2;
+    BluetoothAdapter ba;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b1=(Button)findViewById(R.id.button);
+        b2=(Button)findViewById(R.id.button2);
+        ba=BluetoothAdapter.getDefaultAdapter(); //access blutooth
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ba.enable();
+
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ba.disable();
+
+            }
+        });
+
+    }
+}
